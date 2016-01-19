b5b054e16ef95bd33307ba3884384b3f381cab85
Zoio Silva <rafael.silva@benner.com.br>
2016-01-19 21:56:07 -0200

    Escrevendo aplicação que usarei para testar as coisas.

---
+++ AspectConsole01/AspectConsole01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspectConsole01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Início.");
+
+            FazerCoisas();
+
+            Console.WriteLine("Fim.");
+            Console.ReadKey();
+        }
+
+        private static void FazerCoisas()
+        {
+            var a = new UmaCoisa();
+            a.Fazer();
+
+            var b = new OutraCoisa();
+            b.Fazer(2, 4);
+        }
+    }
+}
---
+++ AspectConsole01/AspectConsole01/UmaCoisa.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspectConsole01
+{
+    public class UmaCoisa
+    {
+        public void Fazer()
+        {
+            Console.WriteLine("Fazendo uma coisa.");
+        }
+    }
+
+    public class OutraCoisa
+    {
+        public void Fazer(int a, int b)
+        {
+            Console.WriteLine("Fazendo outra coisa: {0}, {1};", a, b);
+
+            var a1 = new MaisUmaCoisa().Fazer(a);
+            var a2 = new MaisUmaCoisa().Fazer(b);
+
+            Console.WriteLine("Soma das coisa feitas: {0}", a1 + a2);
+        }
+    }
+
+    public class MaisUmaCoisa
+    {
+        public int Fazer(int parametro1)
+        {
+            Console.WriteLine("Fazendo mais uma coisa: {0};", parametro1);
+            return parametro1 * 2;
+        }
+    }
+}
