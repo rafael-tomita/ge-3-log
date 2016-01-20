8cf62ec2e75519c597b15ab4ec1214332b9e08df
Zoio Silva <rafael.silva@benner.com.br>
2016-01-19 22:19:33 -0200

    Adicionado primeira paradinha orientada a aspectos.

---
+++ AspectConsole01/AspectConsole01/LogAspectAttibute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PostSharp.Aspects;
+
+namespace AspectConsole01
+{
+    [Serializable]
+    public class LogAspectAttibute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Console.WriteLine("Entrou no método {0}.{1}. Argumentos: {2}",
+                args.Method.DeclaringType.Name,
+                args.Method.Name,
+                args.Arguments.Count);
+        }
+    }
+}
--- AspectConsole01/AspectConsole01/UmaCoisa.cs
+++ AspectConsole01/AspectConsole01/UmaCoisa.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace AspectConsole01
 {
+    [LogAspectAttibute]
     public class UmaCoisa
     {
         public void Fazer()
