7aa2de8a58d00d6fb6393e0aaa5c891879051d93
Zoio Silva <rafael.silva@benner.com.br>
2016-01-21 07:52:48 -0200

    Remove and sort usings.

--- AspectConsole01/AspectConsole01/Program.cs
+++ AspectConsole01/AspectConsole01/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AspectConsole01
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
