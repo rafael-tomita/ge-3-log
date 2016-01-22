756997b767abb4858695a48e7d82217b9cde5955
mario-gomes <mario.gomes@benner.com.br>
2016-01-22 08:59:34 -0200

    Projeto base

---
+++ AspectConsole02/Log4Net/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using log4net;
+using log4net.Core;
+
+namespace Log4Net
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = LogManager.GetLogger(
+                System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            if (log.IsDebugEnabled)
+            {
+                log.Debug("Application loaded successfully.");
+            }
+
+            for (var i = 0; i < 100000; i++)
+            {
+                log.Error(i);
+            }
+
+            var Conta = new Somar();
+            var result = Conta.SomarAb(10, 12);
+        }
+    }
+}
---
+++ AspectConsole02/Log4Net/Somar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4Net
+{
+    public class Somar
+    {
+        public int SomarAb(int valorA, int valorB)
+        {
+            return valorA + valorB;
+        }
+    }
+}
